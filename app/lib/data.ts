e10ce5db8b6aeb63c959c03efa802e3dbf7c8b14
DowardCaparas <caparasdounhuward@gmail.com>
2025-04-04 21:57:22 +0800

    add .env

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,10 +1,12 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export const fetchRecipeById = async (id: number) => {
   try {
-    const res = await fetch(`https://dummyjson.com/recipes/${id}`);
+    const res = await fetch(`${API_URL}/recipes/${id}`);
 
     if (!res.ok) throw new Error(`Failed to fetch recipe, ${res.status}`);
 
-    const data = res.json();
+    const data = await res.json();
     return data;
   } catch (error) {
     console.error("Error fetching recipe:", error);
@@ -13,17 +15,15 @@ export const fetchRecipeById = async (id: number) => {
 };
 
 export const fetchAllRecipes = async () => {
-    try {
-        const res =  await fetch('https://dummyjson.com/recipes?limit=0');
+  try {
+    const res = await fetch(`${API_URL}/recipes?limit=0`);
 
-        if(!res.ok) throw new Error(`Failed to fetch recipes, ${res.status}`);
+    if (!res.ok) throw new Error(`Failed to fetch recipes, ${res.status}`);
 
-        const data = await res.json();
-
-        return data.recipes || [];
-
-    } catch (error) {
-        console.error("Error fetching all recipes:", error);
-        throw error;
-    }
-}
+    const data = await res.json();
+    return data.recipes || [];
+  } catch (error) {
+    console.error("Error fetching all recipes:", error);
+    throw error;
+  }
+};
